2e4421ec307ec16daacf2ff8578db3a1e5338fc7
arshiji <64800908+arshiji@users.noreply.github.com>
2023-07-10 20:48:12 -0400

    created directory structure and base code

--- app.py
+++ app.py
@@ -6,65 +6,9 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import re
+from flask import Flask, render_template, jsonify
 
-# Permanently changes the pandas settings
-#pd.set_option('display.max_rows', None)
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.width', None)
-#pd.set_option('display.max_colwidth', -1)
-
-
-# In[80]:
-
-
-def scrape_etsy():
-    url = "https://www.etsy.com/ca/search?q=canada+tshirt&ref=search_bar"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    
-    product_list = []
-    
-    # Find product elements on the page
-    products = soup.find_all("div", class_="v2-listing-card__info")
-    #try:
-    for product in products:
-        # Extract relevant information
-        title = product.find("h3", class_="wt-text-caption v2-listing-card__title wt-text-truncate").text.strip() if product.find("h3", class_="wt-text-caption v2-listing-card__title wt-text-truncate") else None
-        price = product.find("span", class_="currency-value").text.strip() if product.find("span", class_="currency-value") else None
-        currency = product.find("span", class_="currency-symbol").text.strip() if product.find("span", class_="currency-symbol") else None
-        #url = product.find("input", name="listing_url").text.strip()
-        delivery = product.find("div", class_="streamline-spacing-shop-rating").text.strip() if product.find("div", class_="streamline-spacing-shop-rating") elif product.find("span", class_="wt-badge wt-badge--small wt-badge--sale-01").text.strip() else print("Paid")  
-         
-        sales_count = product.find("p", class_="wt-text-title-01 lc-price").text.strip() if product.find("p", class_="wt-text-title-01 lc-price") else None
-        org_prc = product.find("p", class_="wt-text-title-01 lc-price").text.strip() if product.find("p", class_="wt-text-title-01 lc-price") else None
-        if len(org_prc) > 12:
-            sales_count = product.find("p", class_="wt-text-caption search-collage-promotion-price wt-text-slime wt-text-truncate wt-no-wrap")
-            if sales_count:
-                org_prc = sales_count.find("span", class_="currency-value").text.strip() 
-
-        product_data = {
-            "Title": title,
-            "Currency": currency,
-            "Price": price,
-            "Delivery": delivery,
-            "sales_price": org_prc
-        }
-
-        product_list.append(product_data)
-    products = product_list
-
-        
-    return products
-
-# Call the function to scrape Etsy
-etsy_products = scrape_etsy()
-#etsy_products
-df = pd.DataFrame(etsy_products)
-
-
-
-# In[77]:
-
+app = Flask(__name__)
 
 # etsy_449_results
 def scrape_etsy():
@@ -121,4 +65,18 @@ etsy_results = pd.DataFrame(etsy_products)
 
 # Display the DataFrame
 #etsy_results.to_csv("etsy_results.csv")
-etsy_results.count()
+#etsy_results.count()
+
+
+@app.route("/")
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=etsy_results, 
+                           company_name='Arshia Khosla')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(etsy_results)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
